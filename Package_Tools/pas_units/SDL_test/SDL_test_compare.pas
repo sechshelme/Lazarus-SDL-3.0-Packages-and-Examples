863ab8127b77381209386aefe657eefec075c79d
sechshelme <m_burkhard@gmx.ch>
2025-12-08 20:07:07 +0100

    3.3.4

--- Package_Tools/pas_units/SDL_test/SDL_test_compare.pas
+++ Package_Tools/pas_units/SDL_test/SDL_test_compare.pas
@@ -10,6 +10,7 @@ uses
   {$ENDIF}
 
 function SDLTest_CompareSurfaces(surface: PSDL_Surface; referenceSurface: PSDL_Surface; allowable_error: longint): longint; cdecl; external;
+function SDLTest_CompareSurfacesIgnoreTransparentPixels(surface: PSDL_Surface; referenceSurface: PSDL_Surface; allowable_error: Integer): Integer; cdecl; external;
 function SDLTest_CompareMemory(actual: pointer; size_actual: Tsize_t; reference: pointer; size_reference: Tsize_t): longint; cdecl; external;
 
 implementation
