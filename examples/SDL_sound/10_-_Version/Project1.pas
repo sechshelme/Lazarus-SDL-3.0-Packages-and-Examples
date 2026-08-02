1e044630d98c7790bc534ac8343e377869f5d5f9
sechshelme <m_burkhard@gmx.ch>
2026-08-02 16:04:24 +0200

    sdl_sound_3.2.0

--- examples/SDL_sound/10_-_Version/Project1.pas
+++ examples/SDL_sound/10_-_Version/Project1.pas
@@ -4,10 +4,85 @@ uses
   SDL3,
   SDL3_sound;
 
-var
-  version: TSound_Version;
-begin
-  Sound_GetLinkedVersion(@version);
+  procedure main;
+  var
+    sample: PSound_Sample;
+    dev: TSDL_AudioDeviceID;
+    stream: PSDL_AudioStream;
+    decoded_bytes, bytes: TUint32;
+  begin
+    if Sound_Init = 0 then begin
+      SDL_Log('SDL_sound konnte nicht initialisiert werden: %s'#10, Sound_GetError);
+      Exit;
+    end;
 
-  SDL_Log('SDL Sound Version: %d.%d.%d', version.major, version.minor, version.patch);
+    sample := Sound_NewSampleFromFile('/home/tux/Schreibtisch/sound/test.flac', nil, 65536);
+    if sample = nil then begin
+      SDL_Log('Fehler beim Laden: %s'#10, Sound_GetError);
+      Sound_Quit;
+      Exit;;
+    end;
+
+    dev := SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, @sample^.actual);
+    if dev = 0 then begin
+      SDL_Log('Audiogerät konnte nicht geöffnet werden: %s'#10, SDL_GetError);
+      Sound_FreeSample(sample);
+      Sound_Quit;
+      Exit;
+    end;
+
+    stream := SDL_CreateAudioStream(@sample^.actual, nil);
+    if stream = nil then begin
+      SDL_Log('SDL3 AudioStream Fehler: %s'#10, SDL_GetError);
+      SDL_CloseAudioDevice(dev);
+      Sound_FreeSample(sample);
+      Sound_Quit;
+      Exit;
+    end;
+
+    if not SDL_BindAudioStream(dev, stream) then begin
+      SDL_Log('Fehler beim Binden des Streams: %s'#10, SDL_GetError);
+      SDL_DestroyAudioStream(stream);
+      SDL_CloseAudioDevice(dev);
+      Sound_FreeSample(sample);
+      Sound_Quit;
+      Exit;
+    end;
+
+    SDL_ResumeAudioDevice(dev);
+
+    decoded_bytes := Sound_Decode(sample);
+    if decoded_bytes > 0 then begin
+      SDL_PutAudioStreamData(stream, sample^.buffer, decoded_bytes);
+    end;
+
+    // 8. Musik abspielen und im Loop weitere Daten dekodieren (Streaming)
+    SDL_Log('Musik wird abgespielt...'#10);
+    while True do begin
+      if SDL_GetAudioStreamAvailable(stream) < 32768 then begin
+        if (sample^.flags and SOUND_SAMPLEFLAG_EOF) <> 0 or (sample^.flags and SOUND_SAMPLEFLAG_ERROR) then begin
+          if SDL_GetAudioStreamAvailable(stream) = 0 then begin
+            break;
+          end;
+        end else begin
+          bytes := Sound_Decode(sample);
+          if bytes > 0 then begin
+            SDL_PutAudioStreamData(stream, sample^.buffer, bytes);
+          end;
+        end;
+      end;
+      SDL_Delay(10);
+    end;
+    SDL_Log('Wiedergabe beendet.'#10);
+
+    Sound_FreeSample(sample);
+    SDL_DestroyAudioStream(stream);
+    SDL_CloseAudioDevice(dev);
+    Sound_Quit;
+    SDL_Quit;
+  end;
+
+
+begin
+  main;
 end.
--- packages_ext/SDL3_sound/SDL3_sound.pas
+++ packages_ext/SDL3_sound/SDL3_sound.pas
@@ -22,17 +22,16 @@ const
   libSDL3_sound = 'SDL3_sound.dylib';
   {$ENDIF}
 
-  // =====================
-
 const
-  SOUND_VER_MAJOR = 3;
-  SOUND_VER_MINOR = 0;
-  SOUND_VER_PATCH = 0;
+  SDL_SOUND_MAJOR_VERSION = 3;
+  SDL_SOUND_MINOR_VERSION = 2;
+  SDL_SOUND_MICRO_VERSION = 0;
+
+function Sound_Version: longint; cdecl; external libSDL3_sound;
 
 type
   PSound_SampleFlags = ^TSound_SampleFlags;
   TSound_SampleFlags = longint;
-
 const
   SOUND_SAMPLEFLAG_NONE = 0;
   SOUND_SAMPLEFLAG_CANSEEK = 1;
@@ -41,66 +40,58 @@ const
   SOUND_SAMPLEFLAG_EAGAIN = 1 shl 31;
 
 type
-  TSound_AudioInfo = record
-    format: TUint16;
-    channels: TUint8;
-    rate: TUint32;
-  end;
-  PSound_AudioInfo = ^TSound_AudioInfo;
-
-  TSound_DecoderInfo = record
-    extensions: PPAnsiChar;
-    description: PAnsiChar;
-    author: PAnsiChar;
-    url: PAnsiChar;
-  end;
-  PSound_DecoderInfo = ^TSound_DecoderInfo;
   PPSound_DecoderInfo = ^PSound_DecoderInfo;
+  PSound_DecoderInfo = ^TSound_DecoderInfo;
+  TSound_DecoderInfo = record
+    extensions: ^pchar;
+    description: pchar;
+    author: pchar;
+    url: pchar;
+  end;
 
+  PSound_Sample = ^TSound_Sample;
   TSound_Sample = record
     opaque: pointer;
     decoder: PSound_DecoderInfo;
-    desired: TSound_AudioInfo;
-    actual: TSound_AudioInfo;
+    desired: TSDL_AudioSpec;
+    actual: TSDL_AudioSpec;
     buffer: pointer;
     buffer_size: TUint32;
     flags: TSound_SampleFlags;
   end;
-  PSound_Sample = ^TSound_Sample;
 
-  TSound_Version = record
-    major: longint;
-    minor: longint;
-    patch: longint;
-  end;
-  PSound_Version = ^TSound_Version;
-
-procedure SOUND_VERSION(x: PSound_Version);
-
-procedure Sound_GetLinkedVersion(ver: PSound_Version); cdecl; external libSDL3_sound;
 function Sound_Init: longint; cdecl; external libSDL3_sound;
 function Sound_Quit: longint; cdecl; external libSDL3_sound;
 function Sound_AvailableDecoders: PPSound_DecoderInfo; cdecl; external libSDL3_sound;
-function Sound_GetError: PAnsiChar; cdecl; external libSDL3_sound;
+function Sound_GetError: pchar; cdecl; external libSDL3_sound;
 procedure Sound_ClearError; cdecl; external libSDL3_sound;
-function Sound_NewSample(rw: PSDL_IOStream; ext: PAnsiChar; desired: PSound_AudioInfo; bufferSize: TUint32): PSound_Sample; cdecl; external libSDL3_sound;
-function Sound_NewSampleFromMem(data: PUint8; size: TUint32; ext: PAnsiChar; desired: PSound_AudioInfo; bufferSize: TUint32): PSound_Sample; cdecl; external libSDL3_sound;
-function Sound_NewSampleFromFile(fname: PAnsiChar; desired: PSound_AudioInfo; bufferSize: TUint32): PSound_Sample; cdecl; external libSDL3_sound;
+function Sound_NewSample(io: PSDL_IOStream; ext: pchar; desired: PSDL_AudioSpec; bufferSize: TUint32): PSound_Sample; cdecl; external libSDL3_sound;
+function Sound_NewSampleFromMem(data: PUint8; size: TUint32; ext: pchar; desired: PSDL_AudioSpec; bufferSize: TUint32): PSound_Sample; cdecl; external libSDL3_sound;
+function Sound_NewSampleFromFile(filename: pchar; desired: PSDL_AudioSpec; bufferSize: TUint32): PSound_Sample; cdecl; external libSDL3_sound;
 procedure Sound_FreeSample(sample: PSound_Sample); cdecl; external libSDL3_sound;
 function Sound_GetDuration(sample: PSound_Sample): TSint32; cdecl; external libSDL3_sound;
 function Sound_SetBufferSize(sample: PSound_Sample; new_size: TUint32): longint; cdecl; external libSDL3_sound;
+function Sound_SetDesiredFormat(sample: PSound_Sample; desired: PSDL_AudioSpec): longint; cdecl; external libSDL3_sound;
 function Sound_Decode(sample: PSound_Sample): TUint32; cdecl; external libSDL3_sound;
 function Sound_DecodeAll(sample: PSound_Sample): TUint32; cdecl; external libSDL3_sound;
 function Sound_Rewind(sample: PSound_Sample): longint; cdecl; external libSDL3_sound;
 function Sound_Seek(sample: PSound_Sample; ms: TUint32): longint; cdecl; external libSDL3_sound;
 
+function SDL_SOUND_VERSION_ATLEAST(X, Y, Z: integer): boolean; inline;
+function SDL_SOUND_VERSION: longint;
+
 implementation
 
-procedure SOUND_VERSION(x: PSound_Version);
+function SDL_SOUND_VERSION: longint;
 begin
-  x^.major := SOUND_VER_MAJOR;
-  x^.minor := SOUND_VER_MINOR;
-  x^.patch := SOUND_VER_PATCH;
+  SDL_SOUND_VERSION := SDL_VERSIONNUM(SDL_SOUND_MAJOR_VERSION, SDL_SOUND_MINOR_VERSION, SDL_SOUND_MICRO_VERSION);
+end;
+
+function SDL_SOUND_VERSION_ATLEAST(X, Y, Z: integer): boolean; inline;
+begin
+  Result := (SDL_SOUND_MAJOR_VERSION >= X) and
+    ((SDL_SOUND_MAJOR_VERSION > X) or (SDL_SOUND_MINOR_VERSION >= Y)) and
+    ((SDL_SOUND_MAJOR_VERSION > X) or (SDL_SOUND_MINOR_VERSION > Y) or (SDL_SOUND_MICRO_VERSION >= Z));
 end;
 
 end.
